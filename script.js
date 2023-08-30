a6c5cfa2e82bb602e81eb98b9e129135700ddc0a
Oluwasanjo Elisha Adeniyi <143057515+Sanjoclinton@users.noreply.github.com>
2023-08-30 16:36:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.fa-bars').addEventListener('click', () => {
+    document.querySelector('.outer-grid').classList.toggle('outer-grid-expand');
+    document.querySelector('nav').classList.toggle('hide-nav');
+});
+
